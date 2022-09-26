5ae460c151a105e84e5f3ed97e1659b5776dbad0
chylli-binary <chylli@binary.com>
2022-09-26 00:47:55 +0000

    initial commit

---
+++ lib/Test/Release4.pm
@@ -0,0 +1,36 @@
+package Test::Release4;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+Test::Release4 - Module abstract
+
+=head1 SYNOPSIS
+
+    use Test::Release4;
+    my $instance = Test::Release4->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+1;
+
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
