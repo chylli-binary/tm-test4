e1df7faf049abf84a2c1279bbe0fc41e8869b0f9
circleci <circleci@binary.com>
2022-09-26 01:18:01 +0000

    Version 0.002-TRIAL

---
+++ lib/Test/Release4.pm
@@ -0,0 +1,35 @@
+package Test::Release4;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+our $VERSION = '0.002';    # TRIAL
+
+=head1 NAME
+
+Test::Release4 - Module abstract
+
+=head1 SYNOPSIS
+
+    use Test::Release4;
+    my $instance = Test::Release4->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+1;
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
